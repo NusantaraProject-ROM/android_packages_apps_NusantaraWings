cd5be02ef3a9f248ac91a13a80c9369c868b3273
Alex <du.alexcruz@gmail.com>
2019-01-31 19:17:58 -0600

    NadWings: CarrierLabel: Set a text length on the carrier label
    Up til now this has been gone unnoticed. Without a text limit the user
    could very easily blow up the statusbar to the point that is not usable.
    
    Change-Id: I2813af9e12c6d29e020669e0626faec8422fedbe

--- src/com/nusantara/wings/fragments/statusbar/CarrierLabel.java
+++ src/com/nusantara/wings/fragments/statusbar/CarrierLabel.java
@@ -24,6 +24,7 @@ import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.text.InputFilter;
 import android.text.Spannable;
 import android.text.TextUtils;
 import android.widget.EditText;
@@ -113,7 +114,9 @@ public class CarrierLabel extends SettingsPreferenceFragment
 
             // Set an EditText view to get user input
             final EditText input = new EditText(getActivity());
+            int maxLength = 10;
             input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? "" : mCustomCarrierLabelText);
+            input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
             input.setSelection(input.getText().length());
             alert.setView(input);
             alert.setPositiveButton(getString(android.R.string.ok),
