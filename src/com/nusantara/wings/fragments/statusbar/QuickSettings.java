5ff12976ea2ff60e6749baa852474aa2b24a9e11
Genkzsz11 <genkzsz11@nusantaradevs.org>
2020-11-24 19:30:56 +0700

    NadWings: Set default value to 4 for QS panel columns portrait [2/2]

--- src/com/nusantara/wings/fragments/statusbar/QuickSettings.java
+++ src/com/nusantara/wings/fragments/statusbar/QuickSettings.java
@@ -74,7 +74,7 @@ public class QuickSettings extends SettingsPreferenceFragment
         mQsRowsLand.setOnPreferenceChangeListener(this);
 
         value = Settings.System.getIntForUser(resolver,
-                Settings.System.QS_COLUMNS_PORTRAIT, 3, UserHandle.USER_CURRENT);
+                Settings.System.QS_COLUMNS_PORTRAIT, 4, UserHandle.USER_CURRENT);
         mQsColumnsPort = (CustomSeekBarPreference) findPreference("qs_columns_portrait");
         mQsColumnsPort.setValue(value);
         mQsColumnsPort.setOnPreferenceChangeListener(this);
