e4c5c29c46d0adb9c294116bb398c3cab0ab282d
SagarMakhar <sagarmakhar@gmail.com>
2021-12-29 16:19:08 +0000

    NadWings: Move monet settings to seperate fragment
    Change-Id: Ie83e4154acb904aec0452c284111034151d251ac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- src/com/nusantara/wings/fragments/system/Themes.java
+++ src/com/nusantara/wings/fragments/system/Themes.java
@@ -16,13 +16,8 @@
 
 package com.nusantara.wings.fragments.system;
 
-import android.app.Dialog;
-import android.app.UiModeManager;
 import android.content.Context;
 import android.content.ContentResolver;
-import android.content.SharedPreferences;
-import android.content.om.IOverlayManager;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -30,9 +25,6 @@ import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.Toast;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -43,25 +35,14 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
-import com.nusantara.support.colorpicker.ColorPickerPreference;
-import com.nusantara.support.preferences.SystemSettingListPreference;
-import com.nusantara.support.preferences.SystemSettingSwitchPreference;
-import com.nusantara.wings.UtilsNad;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.nusantara.wings.UtilsThemes.handleBackgrounds;
-import static com.nusantara.wings.UtilsThemes.handleOverlays;
 
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class Themes extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
-
-    private String MONET_ENGINE_COLOR_OVERRIDE = "monet_engine_color_override";
-
-    private ColorPickerPreference mMonetColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -69,27 +50,10 @@ public class Themes extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.nad_themes);
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
-
-        mMonetColor = (ColorPickerPreference) prefScreen.findPreference(MONET_ENGINE_COLOR_OVERRIDE);
-        int intColor = Settings.Secure.getInt(resolver, MONET_ENGINE_COLOR_OVERRIDE, Color.WHITE);
-        String hexColor = String.format("#%08x", (0xffffff & intColor));
-        mMonetColor.setNewPreviewColor(intColor);
-        mMonetColor.setSummary(hexColor);
-        mMonetColor.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mMonetColor) {
-            String hex = ColorPickerPreference.convertToARGB(Integer
-                .parseInt(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.Secure.putInt(resolver,
-                MONET_ENGINE_COLOR_OVERRIDE, intHex);
-            return true;
-        }
         return false;
     }
 
