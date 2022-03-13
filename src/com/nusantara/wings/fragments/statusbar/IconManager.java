7277ba18645a8526e2dc2002ffa85d710fdfb0ff
Genkzsz11 <genkzsz11@gmail.com>
2022-03-13 13:00:29 +0700

    NadWings: Bring back some feature

--- src/com/nusantara/wings/fragments/statusbar/IconManager.java
+++ src/com/nusantara/wings/fragments/statusbar/IconManager.java
@@ -50,9 +50,13 @@ import com.nusantara.support.preferences.SecureSettingSwitchPreference;
 public class IconManager extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
+    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+    private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
     private static final String KEY_STATUS_BAR_LOGO = "status_bar_logo";
+    private static final String COBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
 
     private SwitchPreference mShowNadLogo;
+    private SecureSettingSwitchPreference mCombinedIcons;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +70,27 @@ public class IconManager extends SettingsPreferenceFragment
         mShowNadLogo.setChecked((Settings.System.getInt(getContentResolver(),
              Settings.System.STATUS_BAR_LOGO, 0) == 1));
         mShowNadLogo.setOnPreferenceChangeListener(this);
+
+        mCombinedIcons = (SecureSettingSwitchPreference)
+                findPreference(COBINED_STATUSBAR_ICONS);
+        Resources sysUIRes = null;
+        boolean def = false;
+        int resId = 0;
+        try {
+            sysUIRes = getActivity().getPackageManager()
+                    .getResourcesForApplication(SYSTEMUI_PACKAGE);
+        } catch (Exception ignored) {
+            // If you don't have system UI you have bigger issues
+        }
+        if (sysUIRes != null) {
+            resId = sysUIRes.getIdentifier(
+                    CONFIG_RESOURCE_NAME, "bool", SYSTEMUI_PACKAGE);
+            if (resId != 0) def = sysUIRes.getBoolean(resId);
+        }
+        boolean enabled = Settings.Secure.getInt(resolver,
+                COBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1;
+        mCombinedIcons.setChecked(enabled);
+        mCombinedIcons.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -75,6 +100,12 @@ public class IconManager extends SettingsPreferenceFragment
             boolean value = (Boolean) newValue;
             Settings.System.putInt(resolver,
                     Settings.System.STATUS_BAR_LOGO, value ? 1 : 0);
+            return true;
+        } else if (preference == mCombinedIcons) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    COBINED_STATUSBAR_ICONS, enabled ? 1 : 0);
+            UtilsNad.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;
